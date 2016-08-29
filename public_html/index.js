828d57498061b15ef76c0d03a7aa62c3bc55742c
Eleri Apsolon <eleri.apsolon@gmail.com>
2016-08-29 20:38:14 +0300

    Getting familiar with callbacks and promises 2

--- public_html/index.js
+++ public_html/index.js
@@ -9,38 +9,83 @@
 //  throw new badUserName();
 //});
 
-var inner = function (resolve, reject) {
-  //randomly execute resolve or reject kasuta math randomit
-  setTimeout(function () {//pseudo thread
-    var r = Math.random();
-    if (r < 0.5) {
-      resolve('hi');
-    }
-    else {
-      reject('bye');
-    }
-  }, 2000);
-};
+//var inner = function (resolve, reject) {
+//  //randomly execute resolve or reject kasuta math randomit
+//  setTimeout(function () {//pseudo thread
+//    var r = Math.random();
+//    if (r < 0.5) {
+//      resolve('hi');
+//    } else {
+//      reject('bye');
+//    }
+//  }, 2000);
+//};
+//
+//var p = new Promise(inner);
+//
+//var success = function () {
+//  console.log('success', arguments);
+//};
+//
+//var error = function () {
+//  console.log('error', arguments);
+//};
+//
+//p.then(success, error);
+//console.log('jhtju');
+//
+//
+//function callback(i, cb) {
+//  console.log(i);
+//  cb();
+//}
+//
+//callback('Call', function () {
+//  console.log('back');
+//});
 
-var p = new Promise(inner);
 
-var success = function () {
-  console.log('success', arguments);
-};
+//promise to home here in time
+let promiseToBeHereInTime = new Promise(function (resolve, reject) {
+  let isHereInTime = false;
 
-var error = function () {
-  console.log('error', arguments);
-};
-
-p.then(success, error);
-console.log('jhtju');
-
-
-function callback(i, cb) {
-  console.log(i);
-  cb();
-}
-
-callback('Call', function(){
-   console.log('back');
+  if (isHereInTime) {
+    resolve('in time');
+  } else {
+    reject('I am leaving!');
+  }
 });
+
+promiseToBeHereInTime.then(function (resolve) {
+  console.log('You are here ' + resolve);
+}).catch(function (reject) {
+  console.log('You are not here! ' + reject);
+});
+
+//promise after coming
+let isHereInTime = function () {
+  return new Promise(function (resolve, reject) {
+    resolve('You were here on time, ');
+  });
+};
+
+let isSoftwareDev = function(result) {
+  return new Promise(function (resolve, reject) {
+    resolve(result+ 'you are a software developer, ');
+  });
+};
+let hasEnoughKnowledges = function(result) {
+  return new Promise(function (resolve, reject) {
+    resolve(result + 'and you are smart! ');
+  });
+};
+
+isHereInTime().then(function(result) {
+  return isSoftwareDev(result);
+}).then(function(result) {
+  return hasEnoughKnowledges(result);
+}).then(function(result) {
+console.log(result + 'You are hired, Good luck!');
+});
+
+    
